db3129d8307f4a8677cb998b2cfcd3739be92212
nilanjancloud123 <83358199+nilanjancloud123@users.noreply.github.com>
2021-06-10 11:45:51 +0100

    c

--- src/test/java/com/mydemo/BaseTestUpdated.java
+++ src/test/java/com/mydemo/BaseTestUpdated.java
@@ -4,8 +4,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.Method;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.nio.file.DirectoryNotEmptyException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
@@ -13,13 +11,9 @@ import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITest;
 import org.testng.ITestContext;
 import org.testng.ITestResult;
@@ -29,8 +23,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
-import RestPost.JiraExecution;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.cloudExecution.CloudExecution;
+import RestPost.CustomAutomationBot;
 
 
 public class BaseTestUpdated implements ITest {
@@ -50,6 +44,7 @@ public class BaseTestUpdated implements ITest {
     public BaseTestUpdated(){
 	   	    try	
 	   	    {
+	   	    	
 	    	prop= new Properties();
 	    	FileInputStream ip= new FileInputStream(System.getProperty("user.dir")+"/properties/config.properties");
 	    	prop.load(ip);
@@ -126,7 +121,7 @@ public class BaseTestUpdated implements ITest {
 
 	
 	@BeforeSuite
-	public void fileclear() {
+	public void fileclear() throws IOException {
 		String filepath = "\\screenshot";
 		deletefile(filepath);
 		String filepath1 = "\\reports";
@@ -139,8 +134,8 @@ public class BaseTestUpdated implements ITest {
 	// store file path
     //String filepath = "C:\\Users\\nilanjan.islam\\eclipse-workspace\\Demo\\Demo\\screenshot";
 		 
-	
-	  cycleid = JiraExecution.createCycle(tkn,"QTDEM",EndPointCloud) ;
+		
+	  cycleid = CustomAutomationBot.createCloudTesCycle(tkn,"QTDEM",EndPointCloud,"Automation Regression Demo C1") ;
 	  System.out.println("*****CycleID *****" + cycleid);
 	  
 	  
@@ -199,66 +194,17 @@ public class BaseTestUpdated implements ITest {
 		testName.set(methodName+"_"+browserName);
 		
 		ctx.setAttribute("testName", testName.get());
-		//ctx.setAttribute("testName", testName.get());
-		DesiredCapabilities caps = new DesiredCapabilities();
 		if(Target.equals("sauce")) {
-		URL ="https://ondemand.eu-central-1.saucelabs.com:443/wd/hub";
-		MutableCapabilities sauceOpts = new MutableCapabilities();
-		sauceOpts.setCapability("name", testName.get());
-		sauceOpts.setCapability("build", "Java-W3C-Examples");
-		sauceOpts.setCapability("seleniumVersion", "3.141.59");
-		sauceOpts.setCapability("username", "nislam123");
-		sauceOpts.setCapability("accessKey", "bb112c18-f3e4-45ad-bb04-b791dbe81483");
-		sauceOpts.setCapability("tags", "w3c-chrome-tests");
-		caps.setCapability("sauce:options", sauceOpts);
-		caps.setCapability("browserVersion", browser_version);
-		caps.setCapability("platformName", platform);
-		if ((browserName).toUpperCase().equals("CHROME")) {
-			WebDriverManager.chromedriver().setup();
-			caps.setCapability("browserName", "Chrome");
-		} else if ((browserName).toUpperCase().equals(("FIREFOX"))) {
-			WebDriverManager.firefoxdriver().setup();
-			caps.setCapability("browserName", "Firefox");
-		}
+			URL ="https://ondemand.eu-central-1.saucelabs.com:443/wd/hub";
+			driver=CloudExecution.SauceLabSetUp("nislam1234", "360f9b55-3aa0-40a5-b8d4-30a8a206f074", platform, URL, browserName, browser_version, testName.get());
+
 			
 		}else if(Target.equals("browserStack")) {
 			USERNAME = "nilanjanislam_QvESjU";
 			AUTOMATE_KEY = "yzfpaoLLrHRhxANoJ3Tj";
-			BROWSERSTACK_LOCAL= false;
-		    BROWSERSTACK_LOCAL_IDENTIFIER= "identifier";
-			URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
-			caps.setCapability("os", os);
-			caps.setCapability("os_version", os_version);
-			caps.setCapability("browser_version", browser_version);
-			caps.setCapability("name", testName.get());
-			caps.setCapability("browserstack.console", "errors");
-			caps.setCapability("browserstack.debug", "true");
-			caps.setCapability("browserstack.local", BROWSERSTACK_LOCAL);
-			caps.setCapability("browserstack.localIdentifier", BROWSERSTACK_LOCAL_IDENTIFIER);
-			if ((browserName).toUpperCase().equals("CHROME")) {
-				WebDriverManager.chromedriver().setup();
-				caps.setCapability("browser", "Chrome");
-			} else if ((browserName).toUpperCase().equals("FIREFOX")) {
-				WebDriverManager.firefoxdriver().setup();
-				caps.setCapability("browser", "Firefox");
-			}
-			
+			driver=CloudExecution.browserStackSetUp(USERNAME, AUTOMATE_KEY, os, os_version, browserName, browser_version, testName.get());
 		}
-		//
-        
-			
-		try {
-			
-			//driver = new RemoteWebDriver(new URL("https://ondemand.eu-central-1.saucelabs.com:443/wd/hub"), caps);
-			driver = new RemoteWebDriver(new URL(URL), caps);
-			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-			
-		} catch (MalformedURLException e) {
-			markTestStatus("failed",e.toString());
-			e.printStackTrace();
-			
-		}
-
+	
 	}
 	
 	@AfterMethod(alwaysRun = true)
@@ -292,14 +238,14 @@ public class BaseTestUpdated implements ITest {
 		if (result.isSuccess()) {
 			Comment=result.getMethod().getMethodName() + "  Success";
 			Status = "pass";
-			JiraExecution.testExecution(tkn,"QTDEM",EndPointCloud, TestCaseKey, cycleid, Status, Comment);
+			CustomAutomationBot.testExecutionCloud(tkn,"QTDEM",EndPointCloud, TestCaseKey, cycleid, Status, Comment);
 			
 		}else
 		{
 			exceptionMessage1 = Arrays.toString(result.getThrowable().getStackTrace());
 			Comment=exceptionMessage1.replaceAll(",", "<br>");
 			Status = "fail";
-			JiraExecution.testExecution(tkn,"QTDEM",EndPointCloud, TestCaseKey, cycleid, Status, Comment);
+			CustomAutomationBot.testExecutionCloud(tkn,"QTDEM",EndPointCloud, TestCaseKey, cycleid, Status, Comment);
 			
 		}
 		if (target.equals("browserStack") ) {
